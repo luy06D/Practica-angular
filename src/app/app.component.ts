7fb9a003b115a627b8ba3202358321e3d049d31f
Luis David <cusiluis04@gmail.com>
2023-04-24 00:36:49 -0500

    Componentes de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'aprendo-angular';
+  public cards:Array<any> = []
+
+  ngOnInit(): void {
+    this.cards = [
+      {
+        title:"Video 1",
+        subtitle:"Suntitle video 1"
+
+      },
+      {
+        title:"Video 2",
+        subtitle:"Suntitle video "
+
+      },
+      {
+        title:"Video 3",
+        subtitle:"Suntitle video "
+
+      },
+      {
+        title:"Video 3",
+        subtitle:"Suntitle video "
+
+      }
+    ]
+    
+  
+  }
 }
+
